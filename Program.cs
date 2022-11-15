e81da590c472f79d46e03502d1a2e3b7e042b834
Leonardo Andrade <krycvx@gmail.com>
2022-11-15 17:21:26 +0000

    commit tarefa4

--- Program.cs
+++ Program.cs
@@ -34,7 +34,7 @@ namespace ParticipationRoulette
             }
         }
  
-        private static int CaptureInput()
+        private static int Input()
         {
             Console.Write("How many people per group?");
             return Convert.ToInt32(Console.ReadLine());
@@ -120,7 +120,7 @@ namespace ParticipationRoulette
             
             Console.Clear();
 
-            var membersPerTeam = CaptureInput();
+            var membersPerTeam = Input();
 
             var teamCount = students.Count / membersPerTeam;
             var teams = new List<String[]>();
